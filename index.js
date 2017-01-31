76e377a4a6f9ebc2db17b1c541e48e18bb2d56e7
Kyle Maglietta <Kyle Maglietta>
2017-01-31 12:50:08 -0500

    Added JS/AJAX Form upload

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ var faceApp = angular.module('faceApp', ['ngRoute']);
 
 faceApp.controller('MainControler', function
 MainControler($scope){
-  
+  $('#imageForm').ajaxForm(function(data) {
+    if(data.status == 'success') {
+      console.log(data);
+      var myURL = window.location.href;
+      var myDir = myURL.substring( 0, myURL.lastIndexOf( "/" ) + 1);
+      photoURL = myDir + "photo/" + data.file_name;
+    } else if (data.status == 'fail') {
+      //handle error
+      console.log(data.error);
+    }
+  })
 });
